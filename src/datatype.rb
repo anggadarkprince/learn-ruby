1e9b2b1dafabc4afac13b80baf4686fc8aeafaef
Angga Ari Wijaya <anggadarkprince@gmail.com>
2016-08-24 08:23:04 +0700

    moving operator code in datatype.rb into operators.rb

--- src/datatype.rb
+++ src/datatype.rb
@@ -23,18 +23,6 @@ flotingpoint = 3.1433
 binary = 0b101
 hexadec = 0x10
 negative = -230
-operation = 1+1
-substract = 2.2 - 0.43
-multiply = 12 * 23
-division = 12 / 12
-reminder = 3%2
-exponentiation = 2**2
-
-puts(-23.abs)
-puts(12.div(2))
-puts(12.modulo(7))
-puts(100.to_s)
-
 
 #boolean
 cond = 1 > 100
---
+++ src/operators.rb
@@ -0,0 +1,22 @@
+operation = 1+1
+puts(operation)
+
+subtract = 2.2 - 0.43
+puts(subtract)
+
+multiply = 12 * 23
+puts(multiply)
+
+division = 12 / 12
+puts(division)
+
+reminder = 3%2
+puts(reminder)
+
+exponentiation = 2**2
+puts(exponentiation)
+
+puts(-23.abs)
+puts(12.div(2))
+puts(12.modulo(7))
+puts(100.to_s)
