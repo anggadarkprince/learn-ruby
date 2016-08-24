080df8596e3b7c4b788de0f4eeca08e4883d1d71
Angga Ari Wijaya <anggadarkprince@gmail.com>
2016-08-24 08:45:33 +0700

    add formula exercise

---
+++ src/exercises/formula.rb
@@ -0,0 +1,13 @@
+# x / y - 3
+# 1 / (x + y)
+# the square root of x to the 6th plus y to the 5th absolute value of x + y
+
+x = -13
+y = 2.0
+print(x / y - 3)
+puts
+print(1 / (x + y))
+puts
+print(Math.sqrt(x**6 + y**5))
+puts
+print((x + y).abs)
